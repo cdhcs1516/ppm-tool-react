e1d156693d2459dcdd41f0de4bac76e124f38697
cdhcs1516 <503028053@qq.com>
2021-06-20 01:20:20 -0400

    adding essential components and styling

---
+++ src/components/Dashboard.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+import ProjectItem from "./Project/ProjectItem";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a
+                href="ProjectForm.html"
+                className="btn btn-lg btn-info"
+                role="button"
+              >
+                Create a Project
+              </a>
+              <br />
+              <hr />
+
+              <ProjectItem />
+              <ProjectItem />
+              <ProjectItem />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
