01fee195278f09cf870e751038a5cc7d0a7b0f71
cdhcs1516 <503028053@qq.com>
2021-06-20 01:56:41 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import AddProject from "./components/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
