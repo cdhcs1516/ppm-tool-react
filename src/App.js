6aaaded98635c3766bc0dce261c9ac7a56a3b115
cdhcs1516 <503028053@qq.com>
2021-07-07 18:27:53 -0400

    implement update project

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import AddProject from "./components/Project/AddProject";
 import store from "./store";
+import UpdateProject from "./components/Project/UpdateProject";
 
 class App extends Component {
   render() {
@@ -16,8 +17,9 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/addProject" component={AddProject} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/updateProject/:id" component={UpdateProject} />
           </div>
         </Router>
       </Provider>
